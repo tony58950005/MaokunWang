145f96943c597027b47888139963935ec2d9af5f
Akos Nagy <akos.nagy@aut.bme.hu>
2020-05-04 21:02:10 +0200

    Modify the queue class

--- ClassUart_attempt02/Inc/queue.h
+++ ClassUart_attempt02/Inc/queue.h
@@ -5,21 +5,21 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal.h"
 
+#define QUEUE_MAX_SIZE	(256)
+
 #ifdef __cplusplus
 class Queue {
 public:
 	Queue(){};
 	bool IsBuffer_Full();
 	bool IsBuffer_Empty();
-	void Buffer_Write(Queue item);
-	void Buffer_Read();
+	bool Buffer_Write(uint8_t item);
+	bool Buffer_Read(uint8_t* item);
 private:
 	size_t head=0;
 	size_t tail=0;
-	const size_t max_size_;
-	bool full_ =0;//a flag to judge if buffer is full or not. if fill, full_=1;
-
-
+	bool full = false; //a flag to judge if buffer is full or not. if fill, full_=1;
+	uint8_t* buffer[QUEUE_MAX_SIZE]; //Store the queue in this array
 };
 #endif
 #endif
